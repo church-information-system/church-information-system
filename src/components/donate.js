d764c32ea1204e345747adf334d777fe91055a13
Jerome <josejeromelalunio@gmail.com>
2021-11-18 17:53:52 +0800

    display correct amount donated

--- src/components/donate.js
+++ src/components/donate.js
@@ -285,10 +285,10 @@ export default function Donate() {
                 </div>
               </div>
               <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12">AmountDonated</div>
                 <div className="col-lg-6 col-md-6 col-sm-12">
-                  Amount Donated
+                  ₱ {userData["amount"]}
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-12">P 2500.00</div>
               </div>
             </div>
           </div>
