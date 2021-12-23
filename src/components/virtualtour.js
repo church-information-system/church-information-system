11a2c375439f179e11ae1cdb4db45b23239a0a62
villarenteHannah <villarenteHannah22@gmail.com>
2021-12-23 10:01:10 +0800

    add video

--- src/components/virtualtour.js
+++ src/components/virtualtour.js
@@ -15,6 +15,7 @@ import img4 from './../assets/images/img4.jpg';
 import img5 from './../assets/images/img5.jpg';
 import img6 from './../assets/images/img6.jpg';
 import img7 from './../assets/images/img7.jpg';
+import virtual from './../assets/images/virtual.mp4';
 // PANORAMAS
 import pano1 from './../assets/images/churchphoto/pano1.jpg';
 import pano2 from './../assets/images/churchphoto/pano2.jpg';
@@ -39,91 +40,96 @@ export default function VirtualTour() {
             {/* VIRTUAL TOUR PHOTO AND 3D */}
             <div className="row margin">
                 <div className="col-lg-3 col-md-4 col-sm-12">
-                <SimpleReactLightbox>
-                    <SRLWrapper>
-                        <div className="img_line">
-                            <div className="img_item">
-                                <img src={img1} alt="virtualphoto"/>
+                    <SimpleReactLightbox>
+                        <SRLWrapper>
+                            <div className="img_line">
+                                <div className="img_item">
+                                    <img src={img1} alt="virtualphoto" />
+                                </div>
+                                <div className="img_item">
+                                    <img src={img2} alt="virtualphoto" />
+                                </div>
+                                <div className="img_item">
+                                    <img src={img3} alt="virtualphoto" />
+                                </div>
+                                <div className="img_item">
+                                    <img src={img4} alt="virtualphoto" />
+                                </div>
+                                <div className="img_item">
+                                    <img src={img5} alt="virtualphoto" />
+                                </div>
+                                <div className="img_item">
+                                    <img src={img6} alt="virtualphoto" />
+                                </div>
+                                <div className="img_item">
+                                    <img src={img7} alt="virtualphoto" />
+                                </div>
                             </div>
-                            <div className="img_item">
-                                <img src={img2} alt="virtualphoto" />
-                            </div>
-                            <div className="img_item">
-                                <img src={img3} alt="virtualphoto" />
-                            </div>
-                            <div className="img_item">
-                                <img src={img4} alt="virtualphoto" />
-                            </div>
-                            <div className="img_item">
-                                <img src={img5} alt="virtualphoto" />
-                            </div>
-                            <div className="img_item">
-                                <img src={img6} alt="virtualphoto"/>
-                            </div>
-                            <div className="img_item">
-                                <img src={img7} alt="virtualphoto"/>
-                            </div>
-                        </div>
-                       
-                    </SRLWrapper>
-                </SimpleReactLightbox>
+
+                        </SRLWrapper>
+                    </SimpleReactLightbox>
                 </div>
                 <div className="col-lg-9 col-md-8 col-sm-12">
                     <div className="virtualtour">
-                        <iframe src={`https://www.google.com/maps/embed?pb=!4v1627367258818!6m8!1m7!1sCAoSLEFGMVFpcE1OLVQtVGRWM2xWWHRiSHVuSExvcDR6eVhiZmJUcXpTMEhYNTNq!2m2!1d14.01686978068562!2d121.4020784266619!3f200!4f10!5f0.7820865974627469`}
-                            width="100%"
-                            height="800"
-                            display="initial"
-                            position="relative" />
+                        <video
+                            src={virtual}
+                            width="1200px"
+                            height="700px"
+                            className="video"
+                            loop={true}
+                            autoPlay={true}
+                            controls={true}
+                            muted={true}
+                        ></video>
                     </div>
                 </div>
             </div>
             <SimpleReactLightbox>
                 <SRLWrapper>
-                <div className="panorama">
-                    <div className="panorama_item">
-                        <img src={pano1} alt="panorama"/>
+                    <div className="panorama">
+                        <div className="panorama_item">
+                            <img src={pano1} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano2} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano3} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano4} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano5} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano6} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano7} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano8} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano9} alt="panorama" />
+                        </div>
+                        <div className="panorama_item">
+                            <img src={pano10} alt="panorama" />
+                        </div>
                     </div>
-                    <div className="panorama_item">
-                        <img src={pano2} alt="panorama"/>
-                    </div>
-                    <div className="panorama_item">
-                        <img src={pano3} alt="panorama"/>
-                    </div>
-                    <div className="panorama_item">
-                        <img src={pano4} alt="panorama"/>
-                    </div>
-                    <div className="panorama_item">
-                        <img src={pano5} alt="panorama"/>
-                    </div>
-                    <div className="panorama_item">
-                        <img src={pano6} alt="panorama"/>
-                    </div>
-                    <div className="panorama_item">
-                        <img src={pano7} alt="panorama"/>
-                    </div>
-                    <div className="panorama_item">
-                        <img src={pano8} alt="panorama"/>
-                    </div>
-                    <div className="panorama_item">
-                        <img src={pano9} alt="panorama"/>
-                    </div>
-                    <div className="panorama_item">
-                        <img src={pano10} alt="panorama"/>
-                    </div>
-                </div>
                 </SRLWrapper>
             </SimpleReactLightbox>
-            
 
-            
+
+
 
             {/* VIRTUAL TOUR DESCRIPTION */}
             <div className="virtual_desc">
                 <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>                
-            </div>           
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
 
 
             {/* FOOTER */}
